61f2ca186b26fcba656124dbd3aed8863d84fe81
fffaheem <mohdfaheemahmad5@gmail.com>
2021-07-11 05:18:44 +0530

    added button to fetch new Data

--- index.js
+++ index.js
@@ -3,18 +3,17 @@
 let data;
 let textHtml = document.getElementById("text")
 let authorHtml = document.getElementById("author")
+let button = document.getElementById("btn")
+
 function quotes (){
     let randomNo = Math.random()*1643
     let wholeRandom = Math.ceil(randomNo)
-    console.log(wholeRandom)
     if(realData[wholeRandom].author==null){
         realData[wholeRandom].author = "Unknown"
     }
     textHtml.innerText = realData[wholeRandom].text
     authorHtml.innerText = realData[wholeRandom].author
 
-    console.log(realData[wholeRandom].text)
-    console.log(realData[wholeRandom].author)
 }
 
 let getQuotes= async()=>{
@@ -22,14 +21,17 @@ let getQuotes= async()=>{
         let api = "https://type.fit/api/quotes"
         data = await fetch(api)
         realData = await data.json()
-        console.log(data)
         quotes()
-        // console.log(Math.ceil(1.3))
         
     }
     catch(error){
-        // console.log(error)
+        console.log(error)
     }
 }
 getQuotes()
+
+button.addEventListener("click",()=>{
+    quotes() 
+})
+
 
