4811fa466b8854e101e3da282390068499be936c
fffaheem <mohdfaheemahmad5@gmail.com>
2021-07-11 05:05:36 +0530

    fetched api and showing the detail in DOM

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Dynamic Quote Website')
+// console.log('Dynamic Quote Website')
+
+let data;
+let textHtml = document.getElementById("text")
+let authorHtml = document.getElementById("author")
+function quotes (){
+    let randomNo = Math.random()*1643
+    let wholeRandom = Math.ceil(randomNo)
+    console.log(wholeRandom)
+    if(realData[wholeRandom].author==null){
+        realData[wholeRandom].author = "Unknown"
+    }
+    textHtml.innerText = realData[wholeRandom].text
+    authorHtml.innerText = realData[wholeRandom].author
+
+    console.log(realData[wholeRandom].text)
+    console.log(realData[wholeRandom].author)
+}
+
+let getQuotes= async()=>{
+    try{
+        let api = "https://type.fit/api/quotes"
+        data = await fetch(api)
+        realData = await data.json()
+        console.log(data)
+        quotes()
+        // console.log(Math.ceil(1.3))
+        
+    }
+    catch(error){
+        // console.log(error)
+    }
+}
+getQuotes()
+
